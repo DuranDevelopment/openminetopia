7817c9886d2b88c9bb3dd48db63f59bb5ee8dedc
xkaasplakje <57845320+duranaaron@users.noreply.github.com>
2024-09-18 19:21:46 +0200

    Removing debug messages

--- src/main/java/nl/openminetopia/modules/fitness/runnables/FitnessRunnable.java
+++ src/main/java/nl/openminetopia/modules/fitness/runnables/FitnessRunnable.java
@@ -95,15 +95,6 @@ public class FitnessRunnable extends BukkitRunnable {
                 newFlyingFitness +
                 fitnessBoost;
 
-        System.out.println("Drinking: " + minetopiaPlayer.getFitnessGainedByDrinking());
-        System.out.println("Health: " + minetopiaPlayer.getFitnessGainedByHealth());
-        System.out.println("Walking: " + newWalkingFitness);
-        System.out.println("Climbing: " + newClimbingFitness);
-        System.out.println("Sprinting: " + newSprintingFitness);
-        System.out.println("Swimming: " + newSwimmingFitness);
-        System.out.println("Flying: " + newFlyingFitness);
-        System.out.println("Boost: " + fitnessBoost);
-
         if (newTotalFitness > configuration.getMaxFitnessLevel()) newTotalFitness = configuration.getMaxFitnessLevel();
         if (newTotalFitness < 0) newTotalFitness = 0;
 
