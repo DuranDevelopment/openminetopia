bb473bd97019a1e11564cc595664ac47aea6d89b
xkaasplakje <57845320+duranaaron@users.noreply.github.com>
2024-10-03 16:38:55 +0200

    Save config on reload + help format fix

--- src/main/java/nl/openminetopia/modules/core/commands/OpenMinetopiaCommand.java
+++ src/main/java/nl/openminetopia/modules/core/commands/OpenMinetopiaCommand.java
@@ -23,15 +23,19 @@ public class OpenMinetopiaCommand extends BaseCommand {
     public void onReload(Player player) {
         OpenMinetopia.getDefaultConfiguration().getLoader().load();
         OpenMinetopia.setDefaultConfiguration(new DefaultConfiguration(OpenMinetopia.getInstance().getDataFolder()));
+        OpenMinetopia.getDefaultConfiguration().saveConfiguration();
 
         OpenMinetopia.getMessageConfiguration().getLoader().load();
         OpenMinetopia.setMessageConfiguration(new MessageConfiguration(OpenMinetopia.getInstance().getDataFolder()));
+        OpenMinetopia.getMessageConfiguration().saveConfiguration();
 
         OpenMinetopia.getLevelcheckConfiguration().getLoader().load();
         OpenMinetopia.setLevelcheckConfiguration(new LevelCheckConfiguration(OpenMinetopia.getInstance().getDataFolder()));
+        OpenMinetopia.getLevelcheckConfiguration().saveConfiguration();
 
         OpenMinetopia.getColorsConfiguration().getLoader().load();
         OpenMinetopia.setColorsConfiguration(new ColorsConfiguration(OpenMinetopia.getInstance().getDataFolder()));
+        OpenMinetopia.getColorsConfiguration().saveConfiguration();
 
         player.sendMessage(ChatUtils.color("<gold>De configuratiebestanden zijn succesvol herladen!"));
     }
