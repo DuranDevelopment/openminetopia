394bd02de1f93b61254618b3266a04c6680b262e
Daan Plugge <35238334+TheBathDuck@users.noreply.github.com>
2024-10-07 21:51:35 +0200

    Update BankingListCommand.java

--- src/main/java/nl/openminetopia/modules/banking/commands/BankingListCommand.java
+++ src/main/java/nl/openminetopia/modules/banking/commands/BankingListCommand.java
@@ -2,6 +2,7 @@ package nl.openminetopia.modules.banking.commands;
 
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
 import co.aikar.commands.annotation.Subcommand;
 import nl.openminetopia.modules.banking.menu.BankAccountListMenu;
 import org.bukkit.entity.Player;
@@ -10,6 +11,7 @@ import org.bukkit.entity.Player;
 public class BankingListCommand extends BaseCommand {
 
     @Subcommand("list")
+    @CommandPermission("openminetopia.banking.list")
     public void listAccounts(Player player) {
         new BankAccountListMenu().open(player);
     }
